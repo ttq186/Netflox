5fa04854845283359cb948bf99a25c9b9c9edded
Thanh Quang <tt.quang.186@gmail.com>
2023-03-07 11:32:19 +0700

    initial commit

---
+++ src/config.py
@@ -0,0 +1,30 @@
+from typing import Any
+
+from pydantic import BaseSettings, PostgresDsn, RedisDsn
+
+from src.constants import Environment
+
+
+class Config(BaseSettings):
+    DATABASE_URL: PostgresDsn
+    REDIS_URL: RedisDsn
+
+    SITE_DOMAIN: str = "netflox.com"
+
+    ENVIRONMENT: Environment = Environment.PRODUCTION
+
+    CORS_ORIGINS: list[str]
+    CORS_ORIGINS_REGEX: str | None
+    CORS_HEADERS: list[str]
+
+    APP_VERSION: str = "1"
+
+
+settings = Config()
+
+app_configs: dict[str, Any] = {"title": "Netflox API"}
+if settings.ENVIRONMENT.is_deployed:
+    app_configs["root_path"] = f"/v{settings.APP_VERSION}"
+
+if not settings.ENVIRONMENT.is_debug:
+    app_configs["openapi_url"] = None  # hide docs
